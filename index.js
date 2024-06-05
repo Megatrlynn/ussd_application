63136accc6c49939f86de600c84340e1021b0cc4
Megatrlynn <lynnslyna@gmail.com>
2024-06-05 13:51:09 +0200

    commit 3

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post('/ussd', (req, res) => {
     // Determine next action based on user input
     if (userInput.length === 1 && userInput[0] === '') {
         // First level menu: Language selection
-        response = `CON Welcome to the Voting App!\n`;
+        response = `CON Welcome to Mayor voting booth\n`;
         response += `1. English\n`;
         response += `2. Swahili`;
     } else if (userInput.length === 1 && userInput[0] !== '') {
@@ -48,11 +48,11 @@ app.post('/ussd', (req, res) => {
         if (userInput[2] === '1') {
             // Voting option selected
             response = `CON Select a candidate:\n`;
-            response += `1. Candidate 1\n`;
-            response += `2. Candidate 2\n`;
-            response += `3. Candidate 3\n`;
-            response += `4. Candidate 4\n`;
-            response += `5. Candidate 5`;
+            response += `1. Raymond IGABINEZA\n`;
+            response += `2. Florence UMUTONIWASE\n`;
+            response += `3. Jean Paul KWIBUKA\n`;
+            response += `4. Gaella UWAYO\n`;
+            response += `5. Danny HABIMANA`;
         } else if (userInput[2] === '2') {
             // View votes option selected
             response = `END Votes:\n`;
